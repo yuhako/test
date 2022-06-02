4cfd1b7c32cd66ebb625b4c869a43f2924b5e886
yuhako <84523958+yuhako@users.noreply.github.com>
2022-06-02 21:32:08 +0900

    Create myscript.js

---
+++ myscript.js
@@ -0,0 +1,18 @@
+alert('君は深淵に足を踏み入れる覚悟はあるか？');
+
+function play_se(){
+    var bakemono = new Audio('gyokuon.m4a');
+    bakemono.play();
+}
+
+$(function(){
+    $('#button1').click(play_se)
+});
+
+function hurue(){
+    navigator.vibrate([200,100,200,100,200,100,200,100,200,100,200,100,200,100,200,100,])
+}
+
+$(function(){
+    $('#button2').click(hurue)
+});
