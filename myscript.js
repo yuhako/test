9f7bf6bca4cdd1363e6a1bf4717311538f105e34
yuhako <84523958+yuhako@users.noreply.github.com>
2022-06-20 11:58:16 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,3 +1,4 @@
 alert('おまえのまけ');
 
 alert('おまえのまけ');
+alertalert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');alert('おまえのまけ');
