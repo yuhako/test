076e4401e759380d2189312d3abc726bf954f636
yuhako <84523958+yuhako@users.noreply.github.com>
2022-06-20 11:22:40 +0900

    Update myscript.js

--- myscript.js
+++ myscript.js
@@ -1,19 +1,2 @@
-alert('君は深淵に足を踏み入れる覚悟はあるか？');
+alert('おまえのまけ');
 
-function play_se(){
-    var bakemono = new Audio('gyokuon.m4a');
-    bakemono.play();
-}
-
-$(function(){
-    $('#button1').click(play_se)
-});
-
-function hurue(){
-    var karako = new Audio('karaoke.m4a');
-    karako.play();
-}
-
-$(function(){
-    $('#button2').click(hurue)
-});
